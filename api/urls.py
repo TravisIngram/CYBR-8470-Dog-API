038b5a5a096752fd2c2570ddda2f0d0da9b4442c
TravisIngram <ingramtravis@gmail.com>
2021-02-11 18:04:33 -0600

    Add initial urls from other lab

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+from api import controllers
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+urlpatterns = [
+    path('dogs/<int:pk>/', controllers.DogDetail.as_view()),
+    path(r'^dogs', controllers.DogList.as_view()),
+    # path('breeds/<int:pk>/', controllers.BreedDetail.as_view()),
+    # path('breeds', controllers.BreedList.as_view()),
+    path(r'^', include(router.urls)),
+]
