2860450de1be35b37c188551552ca8fe2dfe7bb6
TravisIngram <ingramtravis@gmail.com>
2021-02-12 22:57:14 -0600

    Update urls to include breed routes

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ router = routers.DefaultRouter(trailing_slash=False)
 urlpatterns = [
     path('dogs/<int:pk>/', controllers.DogDetail.as_view()),
     path('dogs', controllers.DogList.as_view()),
-    # path('breeds/<int:pk>/', controllers.BreedDetail.as_view()),
-    # path('breeds', controllers.BreedList.as_view()),
+    path('breeds/<int:pk>/', controllers.BreedDetail.as_view()),
+    path('breeds', controllers.BreedList.as_view()),
     path('', include(router.urls)),
 ]
