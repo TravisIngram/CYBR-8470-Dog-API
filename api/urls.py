2009a2db80d2179123d65579413f8560b29adeda
TravisIngram <ingramtravis@gmail.com>
2021-02-12 22:54:03 -0600

    Update routes with proper paths, remove reg-ex

--- api/urls.py
+++ api/urls.py
@@ -6,8 +6,8 @@ router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('dogs/<int:pk>/', controllers.DogDetail.as_view()),
-    path(r'^dogs', controllers.DogList.as_view()),
+    path('dogs', controllers.DogList.as_view()),
     # path('breeds/<int:pk>/', controllers.BreedDetail.as_view()),
     # path('breeds', controllers.BreedList.as_view()),
-    path(r'^', include(router.urls)),
+    path('', include(router.urls)),
 ]
